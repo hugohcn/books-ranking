c76fcd554eb6523f3fb752262cb41c7c0ac535fe
Hugo Nascimento <nascimento@etus.digital>
2023-10-04 00:58:18 -0300

    chore: adjustments on ad units

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,30 @@
-import Image from 'next/image'
 import BookItem from './components/book-item'
-import Divisor from './components/divisor'
+import books from '../data/books'
 
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col gap-3 justify-start">
-        <h2 className="text-slate-800 font-bold text-xl">Livros Cristaos Recomendados</h2>
+        <h2 className="text-slate-800 font-bold text-xl">Descubra os 5 Livros Cristãos Transformadores que Você Precisa Conhecer Hoje!</h2>
+        <p>Receba Inspiração e Sabedoria com os Melhores Livros Cristãos. Leitura Essencial para Todos.</p>
 
-        <BookItem />
-        {/* <Divisor /> */}
-        <div className="flex justify-center">
-          <div className="join-ads" data-sizes="[[250, 250],[250, 360]]" data-bloco="Content1" data-mobile="1"></div>
-          <div className="join-ads" data-sizes="[[250, 250],[250, 360]]" data-bloco="Content1" data-mobile="0"></div>
-        </div>
+        {books.map((book, index) => (
+          <div key={index}>
+            {index === 0 &&
+              <div key={`ad-unit-top-container-${index}`} className="flex justify-center my-4">
+                <div key={`ad-unit-top-${index}`} className="join-ads" data-bloco="Fixed2" data-mobile="0" data-sizes="[[320, 50]]"></div>
+              </div>
+            }
 
-        <BookItem />
-        {/* <Divisor /> */}
-        <div className="flex justify-center">
-          <div className="join-ads" data-sizes="[[250, 250],[250, 360]]" data-bloco="Content2" data-mobile="1"></div>
-          <div className="join-ads" data-sizes="[[250, 250],[250, 360]]" data-bloco="Content2" data-mobile="0"></div>
-        </div>
+            <BookItem key={`book-${index}`} book={book} />
 
-        <BookItem />
-        {/* <Divisor /> */}
-        <div className="flex justify-center">
-          <div className="join-ads" data-sizes="[[250, 250],[250, 360]]" data-bloco="Content3" data-mobile="1"></div>
-          <div className="join-ads" data-sizes="[[250, 250],[250, 360]]" data-bloco="Content3" data-mobile="0"></div>
-        </div>
-
-        <BookItem />
-
-        <div className="flex justify-center">
-          <div className="join-ads" data-sizes="[[250, 250],[250, 360]]" data-bloco="Content4" data-mobile="1"></div>
-          <div className="join-ads" data-sizes="[[250, 250],[250, 360]]" data-bloco="Content4" data-mobile="0"></div>
-        </div>
+            <div key={`ad-unit-content-container-${index}`} className="flex justify-center my-4">
+              <div key={`ad-unit-mobile-${index}`} className="join-ads" data-sizes="[[300, 250], [336,280]]" data-bloco={`Content${index}`} data-mobile="1"></div>
+              <div key={`ad-unit-desk-${index}`} className="join-ads" data-sizes="[[300, 250], [336,280]]" data-bloco={`Content${index}`} data-mobile="0"></div>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
-
   )
 }
---
+++ src/data/books.js
@@ -0,0 +1,29 @@
+
+const books = [
+  {
+    name: 'ALFA E ÔMEGA: O Cristo que Tudo Sustenta e Tudo Completa',
+    author: 'Charles Spurgeon',
+    thumbnail: 'https://m.media-amazon.com/images/I/818w1CZhGRL._SY466_.jpg',
+    url: 'https://amzn.to/3totZc9',
+    price: 0.00,
+    pages_count: 29
+  },
+  {
+    name: 'MANTENDO A ALMA VIVA: Descobrindo a Verdadeira Vida em Cristo',
+    author: 'Charles Spurgeon',
+    thumbnail: 'https://m.media-amazon.com/images/I/71vrpFxk7DL._SY466_.jpg',
+    url: 'https://amzn.to/3RIz0GG',
+    price: 0.00,
+    pages_count: 45
+  },
+  {
+    name: 'ORANDO COMO JACÓ: Desvendando os segredos de uma oração eficaz',
+    author: 'Charles Spurgeon',
+    thumbnail: 'https://m.media-amazon.com/images/I/71zCN+W1nPL._SY466_.jpg',
+    url: 'https://amzn.to/3F7sC4c',
+    price: 0.00,
+    pages_count: 56
+  },
+]
+
+export default books
